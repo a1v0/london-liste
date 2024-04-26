b42c2d6b23c508d7459f8e8b31ce5bec5ac195b6
a1v0 <avc285@gmail.com>
2024-04-26 20:06:09 +0100

    Add Bodoni font to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		fontFamily: { serif: ["Bodoni Moda", "serif"] },
+		extend: {},
+	},
+	plugins: [],
+};
