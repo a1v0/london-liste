2e6ad90dc2ce81c6858d81573895730e0a71b39a
a1v0 <avc285@gmail.com>
2024-04-26 21:45:06 +0100

    Create placeholder `places` file

---
+++ src/places.js
@@ -0,0 +1,15 @@
+exports.places = [
+	/**
+     
+To create a new place, paste this template after the last place below:
+
+    {
+        name: "",
+        slug: "", // leave this blank
+    },
+
+
+
+
+     */
+];
