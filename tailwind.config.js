cd86faa781ca06e94378c3a64961725e2c3e9e3e
a1v0 <avc285@gmail.com>
2024-04-26 20:40:02 +0100

    Add green theme colour to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
 		fontFamily: { serif: ["Bodoni Moda", "serif"] },
+		colors: { emerald: "#004d24" },
 		extend: {},
 	},
 	plugins: [],
