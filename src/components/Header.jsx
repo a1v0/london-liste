19e88f6ec508a78119fe6384e50c2f3259672d96
a1v0 <avc285@gmail.com>
2024-04-26 20:20:23 +0100

    Create `Header`

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "./Logo";
+import Nav from "./Nav";
+
+export default function Header() {
+	return (
+		<div>
+			<Logo />
+			<Nav />
+		</div>
+	);
+}
