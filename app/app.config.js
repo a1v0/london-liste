7762d62242e4496069537e9051b7d51a5f13f9bb
a1v0 <avc285@gmail.com>
2024-04-27 23:09:03 +0100

    Add `placeTypes` to global variables

--- app/app.config.js
+++ app/app.config.js
@@ -1,5 +1,7 @@
 const { places } = require("./places");
+const { getPlacesOfType, getPlaceTypes } = require("./places-utils");
 
 module.exports = {
 	allPlaces: places,
+	placeTypes: getPlaceTypes(places),
 };
