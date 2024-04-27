b2fe4ee678f7a8405a6867d6b877512e7ba1325a
a1v0 <avc285@gmail.com>
2024-04-27 23:25:42 +0100

    Set gutter width in `Header`

--- app/Header.jsx
+++ app/Header.jsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 export default function Header() {
 	return (
-		<header className="flex">
+		<header className="flex clear-gutters">
 			<Logo />
 			<Nav />
 		</header>
