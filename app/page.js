4d9489dfce9a83a56e2fe4919e1425b8e3115248
a1v0 <avc285@gmail.com>
2024-04-27 19:34:07 +0100

    Add dummy text to `page.js`

--- app/page.js
+++ app/page.js
@@ -1,3 +1,48 @@
 export default function Home() {
-	return <main></main>;
+	return (
+		<main>
+			page content lorem ipsum dolor sit amet page content lorem ipsum
+			dolor sit amet page content lorem ipsum dolor sit amet page content
+			lorem ipsum dolor sit amet page content lorem ipsum dolor sit amet
+			page content lorem ipsum dolor sit amet page content lorem ipsum
+			dolor sit amet page content lorem ipsum dolor sit amet page content
+			lorem ipsum dolor sit amet page content lorem ipsum dolor sit amet
+			page content lorem ipsum dolor sit amet page content lorem ipsum
+			dolor sit amet page content lorem ipsum dolor sit amet page content
+			lorem ipsum dolor sit amet page content lorem ipsum dolor sit amet
+			page content lorem ipsum dolor sit amet page content lorem ipsum
+			dolor sit amet page content lorem ipsum dolor sit amet page content
+			lorem ipsum dolor sit amet page content lorem ipsum dolor sit amet
+			page content lorem ipsum dolor sit amet page content lorem ipsum
+			dolor sit amet page content lorem ipsum dolor sit amet page content
+			lorem ipsum dolor sit amet page content lorem ipsum dolor sit amet
+			page content lorem ipsum dolor sit amet page content lorem ipsum
+			dolor sit amet page content lorem ipsum dolor sit amet page content
+			lorem ipsum dolor sit amet page content lorem ipsum dolor sit amet
+			page content lorem ipsum dolor sit amet page content lorem ipsum
+			dolor sit amet page content lorem ipsum dolor sit amet page content
+			lorem ipsum dolor sit amet page content lorem ipsum dolor sit amet
+			page content lorem ipsum dolor sit amet page content lorem ipsum
+			dolor sit amet page content lorem ipsum dolor sit amet page content
+			lorem ipsum dolor sit amet page content lorem ipsum dolor sit amet
+			page content lorem ipsum dolor sit amet page content lorem ipsum
+			dolor sit amet page content lorem ipsum dolor sit amet page content
+			lorem ipsum dolor sit amet page content lorem ipsum dolor sit amet
+			page content lorem ipsum dolor sit amet page content lorem ipsum
+			dolor sit amet page content lorem ipsum dolor sit amet page content
+			lorem ipsum dolor sit amet page content lorem ipsum dolor sit amet
+			page content lorem ipsum dolor sit amet page content lorem ipsum
+			dolor sit amet page content lorem ipsum dolor sit amet page content
+			lorem ipsum dolor sit amet page content lorem ipsum dolor sit amet
+			page content lorem ipsum dolor sit amet page content lorem ipsum
+			dolor sit amet page content lorem ipsum dolor sit amet page content
+			lorem ipsum dolor sit amet page content lorem ipsum dolor sit amet
+			page content lorem ipsum dolor sit amet page content lorem ipsum
+			dolor sit amet page content lorem ipsum dolor sit amet page content
+			lorem ipsum dolor sit amet page content lorem ipsum dolor sit amet
+			page content lorem ipsum dolor sit amet page content lorem ipsum
+			dolor sit amet page content lorem ipsum dolor sit amet page content
+			lorem ipsum dolor sit amet page content lorem ipsum dolor sit amet
+		</main>
+	);
 }
