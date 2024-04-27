cdcfe9cd269aff1f8e16bfe5f27c147614df14b6
a1v0 <avc285@gmail.com>
2024-04-27 19:33:43 +0100

    Remove colouring from logo

--- app/Logo.jsx
+++ app/Logo.jsx
@@ -1,9 +1,7 @@
 export default function Logo() {
 	return (
 		<div>
-			<h1 className="text-emerald font-bold font-fraktur text-5xl">
-				London-Liste
-			</h1>
+			<h1 className="font-bold font-fraktur text-5xl">London-Liste</h1>
 		</div>
 	);
 }
