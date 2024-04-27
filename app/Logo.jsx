2dedb07adeef093ead898c92a709f035b4cdc289
a1v0 <avc285@gmail.com>
2024-04-27 17:51:23 +0100

    Remove obsolete `react` import

--- app/Logo.jsx
+++ app/Logo.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-
 export default function Logo() {
 	return (
 		<div>
-			<h1 className="text-emerald font-serif">Boobies!</h1>
+			<h1 className="text-emerald font-bold font-serif text-xl">
+				London-Liste
+			</h1>
 		</div>
 	);
 }
