3a8771486138d1ecf141a612e5666cb8fedb3534
a1v0 <avc285@gmail.com>
2024-04-27 19:58:18 +0100

    Add instructions for creating new places

--- app/places.js
+++ app/places.js
@@ -1,15 +1,29 @@
 exports.places = [
 	/**
-     
-To create a new place, paste this template after the last place below:
+     * Approved place types: museum, restaurant, landmark, park
+     * 
+     * Location examples:
+     * - Buckingham Palace: "Central - Green Park"
+     * - Pitzhanger: "West - Ealing"
+     * - Dulwich Picture Gallery: "South - Dulwich"
+     * - St Albans: "Outside London - St Albans"
+     * 
+     * To create a new place, paste this template after the last place below:
 
     {
-        name: "",
         slug: "", // leave this blank
+        name: "",
+        keyFacts: [
+            "",
+            "", // add more or delete as necessary
+        ],
+        description: "",
+        type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+        cost: "",
+        location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+        website: ""
     },
 
-
-
-
      */
+	// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< DO NOT PASTE ANYTHING BELOW THIS LINE
 ];
