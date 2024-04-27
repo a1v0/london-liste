41ef70a07d183f052d2acf850152db6e5798351e
a1v0 <avc285@gmail.com>
2024-04-27 20:01:57 +0100

    Add sample place

--- app/places.js
+++ app/places.js
@@ -25,5 +25,20 @@ exports.places = [
     },
 
      */
+	{
+		slug: "", // leave this blank
+		name: "Osterley House",
+		keyFacts: [
+			"Pristine Georgian country house",
+			"House closes during winter. Check website before planning visit", // add more or delete as necessary
+		],
+		description:
+			"A beautifully kept example of a Georgian country house. Gardens are gorgeous, too. A great day out.",
+		type: "museum", // see list of approved types above. Let a1v0 know if you need to add another type.
+		cost: "ca. £15pp. Check website.",
+		location: "West - Osterley", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+		website:
+			"https://www.nationaltrust.org.uk/visit/london/osterley-park-and-house",
+	},
 	// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< DO NOT PASTE ANYTHING BELOW THIS LINE
 ];
