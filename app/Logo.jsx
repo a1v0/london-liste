e6934efaeaec3247d1059e9e091ac48b62f02516
a1v0 <avc285@gmail.com>
2024-04-28 18:23:55 +0100

    Remove `h`-tag from `Logo`

--- app/Logo.jsx
+++ app/Logo.jsx
@@ -1,9 +1,7 @@
 export default function Logo() {
 	return (
-		<div className="py-1">
-			<h1 className="font-bold font-fraktur text-5xl text-[white]">
-				London-Liste
-			</h1>
+		<div className="py-1 font-bold font-fraktur text-5xl text-[white]">
+			London-Liste
 		</div>
 	);
 }
