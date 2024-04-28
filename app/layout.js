34b4254a9dd6db3b91d896967777041f870cb1cd
a1v0 <avc285@gmail.com>
2024-04-28 09:18:50 +0100

    Fix `crossOrigin` bug

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
 				<link
 					rel="preconnect"
 					href="https://fonts.gstatic.com"
-					crossorigin
+					crossOrigin
 				/>
 				<link
 					href="https://fonts.googleapis.com/css2?family=UnifrakturMaguntia&display=swap"
