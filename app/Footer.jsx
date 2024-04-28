0de354963dd89c05f2f805858ec8d08aab98c810
a1v0 <avc285@gmail.com>
2024-04-28 19:53:24 +0100

    Create footer content

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Footer() {
-	return <footer>Footer placeholder</footer>;
+	return (
+		<footer className="bg-brand-blue py-2">
+			<div className="clear-gutters text-[white]">
+				Copyright &copy; {new Date().getFullYear() + " "}
+				<strong>
+					<Link href="https://github.com/a1v0/">a1v0</Link>
+				</strong>
+				. All rights reserved.
+			</div>
+		</footer>
+	);
 }
