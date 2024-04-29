5dedacd2a5488285f99f0280a4a203549231bb69
a1v0 <avc285@gmail.com>
2024-04-29 21:31:34 +0100

    Create rough page layout for place

--- app/places/[slug]/page.js
+++ app/places/[slug]/page.js
@@ -1,5 +1,7 @@
 import { notFound } from "next/navigation";
 import GlobalConfig from "./../../app.config.js";
+import Link from "next/link.js";
+import PageLoader from "next/dist/client/page-loader.js";
 
 export default function Place({ params: { slug } }) {
 	const place = GlobalConfig.allPlaces.find((item) => {
@@ -9,5 +11,36 @@ export default function Place({ params: { slug } }) {
 		return notFound();
 	}
 
-	return <div></div>;
+	return (
+		<div>
+			<article className="clear-gutters">
+				<h1>{place.name}</h1>
+				{place.website ? (
+					<p>
+						(<Link href={place.website}>website</Link>)
+					</p>
+				) : (
+					""
+				)}
+				<ul>
+					{place.keyFacts.map((fact, index) => {
+						return (
+							<li key={index} className="list-disc ml-4">
+								{fact}
+							</li>
+						);
+					})}
+				</ul>
+
+				<p>
+					<strong>Location:</strong> {place.location}
+				</p>
+				<p>
+					<strong>Cost:</strong> {place.cost}
+				</p>
+
+				<p>{place.description}</p>
+			</article>
+		</div>
+	);
 }
