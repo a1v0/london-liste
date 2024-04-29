837f978c9fb075b4ad1b563d89aee625312d2aa8
a1v0 <avc285@gmail.com>
2024-04-29 21:13:48 +0100

    Create basic 404 page

---
+++ app/not-found.jsx
@@ -0,0 +1,14 @@
+export default function NotFoundPage() {
+	return (
+		<main className="flex grow flex-col">
+			<div className="bg-accent-colour">
+				<header className="clear-gutters py-16">
+					<h1 className="m-auto p-0">
+						<span className="block">Page not found.</span>
+						<span className="block">Better luck next time.</span>
+					</h1>
+				</header>
+			</div>
+		</main>
+	);
+}
