d0566bae1d6b4c3286bca04d65fa58b0e0103d64
a1v0 <avc285@gmail.com>
2024-04-29 21:44:51 +0100

    Add link to logo

--- app/Logo.jsx
+++ app/Logo.jsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Logo() {
 	return (
 		<div className="py-1 font-bold font-fraktur text-5xl text-[white]">
-			London-Liste
+			<Link href="/">London-Liste</Link>
 		</div>
 	);
 }
