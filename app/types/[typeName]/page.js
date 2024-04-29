1649432d07ba25371b7bccbc3ea623b4d95823c4
a1v0 <avc285@gmail.com>
2024-04-29 22:06:01 +0100

    Print list of all places to types page

--- app/types/[typeName]/page.js
+++ app/types/[typeName]/page.js
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
 import GlobalConfig from "../../app.config.js";
-import Link from "next/link.js";
+import PlaceTile from "@/app/PlaceTile.jsx";
 
 export default function PlaceType({ params: { typeName } }) {
 	const placeType = GlobalConfig.placeTypes[typeName];
@@ -11,7 +11,10 @@ export default function PlaceType({ params: { typeName } }) {
 	return (
 		<div>
 			<article className="clear-gutters">
-				<h1>hello</h1>{" "}
+				<h1>{placeType[0].type}s</h1>
+				{placeType.map((place) => {
+					return <PlaceTile place={place} />;
+				})}
 			</article>
 		</div>
 	);
