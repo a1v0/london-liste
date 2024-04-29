7378e50f05ffc560b400ce2cbf7cf7413f9d9800
a1v0 <avc285@gmail.com>
2024-04-29 22:12:19 +0100

    Create basic places-by-type page

---
+++ app/types/page.js
@@ -0,0 +1,27 @@
+import PlaceTile from "../PlaceTile.jsx";
+import GlobalConfig from "../app.config.js";
+
+export default function PlaceTypes() {
+	const placesByType = GlobalConfig.placeTypes;
+	const placeTypes = Object.keys(placesByType);
+
+	return (
+		<div>
+			<article className="clear-gutters">
+				<h1>Places by Type</h1>
+
+				{placeTypes.map((placeType) => {
+					const placesOfType = placesByType[placeType];
+					return (
+						<div key={placeType}>
+							<h2 id={placeType}>{placeType}</h2>
+							{placesOfType.map((place) => {
+								return <PlaceTile place={place} />;
+							})}
+						</div>
+					);
+				})}
+			</article>
+		</div>
+	);
+}
