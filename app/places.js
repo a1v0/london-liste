cb066cddc64cc6473c0857bbc550df9152fe7017
a1v0 <avc285@gmail.com>
2024-04-30 21:31:04 +0100

    Comment out unused templates

--- app/places.js
+++ app/places.js
@@ -116,320 +116,319 @@ exports.places = [
 		location: "Central - Euston", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
 		website: "https://www.ucl.ac.uk/culture/petrie-museum",
 	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
-	{
-		slug: "", // leave this blank
-		name: "",
-		keyFacts: [
-			"",
-			"", // add more or delete as necessary
-		],
-		description: "",
-		type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-		cost: "",
-		location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-		website: "",
-	},
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
+	// {
+	// 	slug: "", // leave this blank
+	// 	name: "",
+	// 	keyFacts: [
+	// 		"",
+	// 		"", // add more or delete as necessary
+	// 	],
+	// 	description: "",
+	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
+	// 	cost: "",
+	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+	// 	website: "",
+	// },
 
-	// Petrie Museum
 	// Hunterian Museum
 	// London Walk (there are some very good ones, just Google them. A Beatles walk, a Jack the Ripper walk etc. and they cost about five pounds)
 	// Excursion to Oxford or Cambridge
