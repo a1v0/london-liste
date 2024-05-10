5f3a6794edc8f39a6abaa8a02c4c79d749784b2b
a1v0 <avc285@gmail.com>
2024-05-10 15:31:06 +0100

    Add Sightseeing on a red bus

--- app/places.js
+++ app/places.js
@@ -263,19 +263,19 @@ exports.places = [
 	 	location: "Central - everywhere", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
 	 	website: "https://tfl.gov.uk/modes/cycling/santander-cycles",
 	},
-	// {
-	// 	slug: "", // leave this blank
-	// 	name: "",
-	// 	keyFacts: [
-	// 		"",
-	// 		"", // add more or delete as necessary
-	// 	],
-	// 	description: "",
-	// 	type: "", // see list of approved types above. Let a1v0 know if you need to add another type.
-	// 	cost: "",
-	// 	location: "", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
-	// 	website: "",
-	// },
+	{
+	 	slug: "london-bus-tour", // leave this blank
+	 	name: "Sightseeing on a red bus",
+	 	keyFacts: [
+	 		"See the sights for the cost of a normal bus fare",
+	 		"Check out the website for a list of all leisure routes. They're ordinary bus routes that go along all of the important tourist spots", // add more or delete as necessary
+	 	],
+	 	description: "",
+	 	type: "activity", // see list of approved types above. Let a1v0 know if you need to add another type.
+	 	cost: "ca. £2",
+		location: "Central - various", // "[region] - [area]" or "[region] - [Tube stop]". For examples, see above.
+		website: "https://tfl.gov.uk/travel-information/visiting-london/experience-london/bus-leisure-routes",
+	},
 	// {
 	// 	slug: "", // leave this blank
 	// 	name: "",
@@ -434,7 +434,6 @@ exports.places = [
 	// },
 
 	// ACTIVITIES
-	// Barclays Bike
 	// Looking for Roman shards at the Millennium Bridge
 	// London Walk (there are some very good ones, just Google them. A Beatles walk, a Jack the Ripper walk etc. and they cost about five pounds)
 	// The Monument (two pounds for students, but one must climb 311 steps)
