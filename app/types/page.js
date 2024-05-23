f8732e5c692cda8f1e4e8415110fa82b002ac90e
a1v0 <avc285@gmail.com>
2024-05-23 21:07:19 +0100

    Style list view to be more legible

--- app/types/page.js
+++ app/types/page.js
@@ -24,14 +24,24 @@ export default function PlaceTypes() {
 				{placeTypes.map((placeType) => {
 					const placesOfType = placesByType[placeType];
 					return (
-						<div key={placeType}>
-							<h2 id={placeType}>{placeType}</h2>
-							{placesOfType.map((place) => {
-								return (
-									<PlaceTile key={place.slug} place={place} />
-								);
-							})}
-						</div>
+						<section key={placeType}>
+							<h2
+								id={placeType}
+								className={`border-b-2 border-brand-blue border-dotted`}
+							>
+								{placeType}
+							</h2>
+							<div className={`px-4`}>
+								{placesOfType.map((place) => {
+									return (
+										<PlaceTile
+											key={place.slug}
+											place={place}
+										/>
+									);
+								})}
+							</div>
+						</section>
 					);
 				})}
 			</article>
