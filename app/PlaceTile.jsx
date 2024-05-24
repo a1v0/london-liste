22dcc7ba78d93bfcf03e41fb86f8cf530bcabc9a
a1v0 <avc285@gmail.com>
2024-05-24 21:44:27 +0100

    Remove underlining from PlaceTile

--- app/PlaceTile.jsx
+++ app/PlaceTile.jsx
@@ -2,7 +2,7 @@ import Link from "next/link.js";
 
 export default function PlaceTile({ place }) {
 	return (
-		<Link href={`/places/${place.slug}`} key={place.slug} className={`group`}>
+		<Link href={`/places/${place.slug}`} key={place.slug} className={`group place-tile`}>
 			<div className={`p-2 border-[white] group-hover:border-brand-blue rounded-md border-2 group-hover:bg-light-grey`}>
 				<h3 className={`group-hover:underline`}>{place.name}</h3>
 				<p className="uppercase">{place.type}</p>
