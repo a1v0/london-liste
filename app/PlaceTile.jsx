7fa3e336b169028b34d19ca0322feb6683b1e69c
a1v0 <avc285@gmail.com>
2024-05-24 21:41:49 +0100

    Add hover styles to tile

--- app/PlaceTile.jsx
+++ app/PlaceTile.jsx
@@ -2,9 +2,9 @@ import Link from "next/link.js";
 
 export default function PlaceTile({ place }) {
 	return (
-		<Link href={`/places/${place.slug}`} key={place.slug}>
-			<div>
-				<h3>{place.name}</h3>
+		<Link href={`/places/${place.slug}`} key={place.slug} className={`group`}>
+			<div className={`p-2 border-[white] group-hover:border-brand-blue rounded-md border-2 group-hover:bg-light-grey`}>
+				<h3 className={`group-hover:underline`}>{place.name}</h3>
 				<p className="uppercase">{place.type}</p>
 				<p>{place.keyFacts[0]}</p>
 			</div>
